8403562a378aba39796932d6525cc9797c2529ac
summer <80736033+yl9517@users.noreply.github.com>
2023-10-16 20:03:08 +0900

    상속받아 서브클래스에서 각 amount 실행

--- src/service/PerformanceCalculator.js
+++ src/service/PerformanceCalculator.js
@@ -5,26 +5,9 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 1000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르:${this.play.type}`);
-    }
-    return result;
+    throw new Error("서브 클래스에서 처리하도록 설계되었습니다.");
   }
+
   get volumeCredits() {
     let result = 0;
     result += Math.mac(this.performance.audience - 30, 0);
--- src/service/createStatementData.js
+++ src/service/createStatementData.js
@@ -1,7 +1,32 @@
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르:${aPlay.type}`);
+  }
 }
-
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 1000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+}
 function enrichPerformance(aPerformance) {
   const calculator = createPerformanceCalculator(
     aPerformance,
